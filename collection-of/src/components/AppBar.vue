3266b9e65e6a1913605a7cd0b6c673531bf3b3a3
zer0ken <a37592777@gmail.com>
2022-09-21 23:24:57 +0900

    appbar and main page

---
+++ collection-of/src/components/AppBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app-bar app>
+    <v-container class="text-center">
+        <v-toolbar-title>Collection of</v-toolbar-title>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "AppBar",
+};
+</script>
---
+++ collection-of/src/components/MainBoard.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container class="text-center">test</v-container>
+</template>
+
+<script>
+export default {
+  name: "MainBoard",
+};
+</script>
