8c37bd04be1c2861178ab95ae4e078749f429a15
zer0ken <a37592777@gmail.com>
2022-09-23 22:29:01 +0900

    router

--- collection-of/src/router/index.js
+++ collection-of/src/router/index.js
@@ -1,18 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import TopicView from '../views/TopicView.vue'
 
 Vue.use(VueRouter)
 
-export default new VueRouter({
+const routes = [{
+    path: '/',
+    redirect: '/home'
+}, {
+    path: '/home',
+    component: HomeView
+}, {
+    path: '/:topic',
+    component: TopicView
+}]
+
+const router = new VueRouter({
     mode: 'history',
-    routes: [{
-        path: '/',
-        redirect: '/home'
-    }, {
-        path: '/home',
-        component: () => import('../views/HomeView.vue')
-    }, {
-        path: '/topic/:id',
-        component: () => import('../views/TopicView.vue')
-    }]
+    routes: routes
 })
+
+export default router
